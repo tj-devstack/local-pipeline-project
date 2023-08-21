cd70117f40b9c343deff8e35fb33cc672ef23bde
tj-devstack <tjpark@devstack.co.kr>
2023-08-21 12:50:39 +0800

    test

---
+++ LineDetectPID.py
@@ -0,0 +1,81 @@
+# ROSMASTER params
+{
+    "Hmin": {
+        "default": 5,
+        "min": 0,
+        "max": 180,
+        "description": "Hmin in HSV"
+    },
+    "Smin": {
+        "default": 75,
+        "min": 0,
+        "max": 255,
+        "description": "Smin in HSV"
+    },
+    "Vmin": {
+        "default": 126,
+        "min": 0,
+        "max": 255,
+        "description": "Vmin in HSV"
+    },
+    "Hmax": {
+        "default": 9,
+        "min": 0,
+        "max": 180,
+        "description": "Hmax in HSV"
+    },
+    "Smax": {
+        "default": 253,
+        "min": 0,
+        "max": 255,
+        "description": "Smax in HSV"
+    },
+    "Vmax": {
+        "default": 255,
+        "min": 0,
+        "max": 255,
+        "description": "Vmax in HSV"
+    },
+    "scale": {
+        "default": 1000,
+        "min": 0,
+        "max": 1000,
+        "description": "scale"
+    },
+    "Kp": {
+        "default": 60,
+        "min": 0,
+        "max": 100,
+        "description": "Kp in PID"
+    },
+    "Ki": {
+        "default": 0,
+        "min": 0,
+        "max": 100,
+        "description": "Ki in PID"
+    },
+    "Kd": {
+        "default": 20,
+        "min": 0,
+        "max": 100,
+        "description": "Kd in PID"
+    },
+    "linear": {
+        "default": 0.2,
+        "min": 0,
+        "max": 1.0,
+        "description": "linear"
+    },
+    "LaserAngle": {
+        "default": 30,
+        "min": 10,
+        "max": 90,
+        "description": "LaserAngle"
+    },
+    "ResponseDist": {
+        "default": 0.55,
+        "min": 0,
+        "max": 8,
+        "description": "ResponseDist"
+    }
+}
